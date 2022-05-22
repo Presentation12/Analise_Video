6ef77c86c743c43fdaaa2c1a51948e2b2344c04c
Pedro Simões <69718437+L0ud3r@users.noreply.github.com>
2022-05-22 18:42:13 +0100

    Segmenta minimamente bem

--- Analise_Video_Frame/main.c
+++ Analise_Video_Frame/main.c
@@ -4,11 +4,29 @@
 
 
 int main(){
-    IVC* image = vc_read_image("frame1.png");
+    IVC* src = vc_read_image("frame10.ppm");
+    IVC* hsv = vc_image_new(src->width,src->height,3,255);
+    IVC* hsv_s = vc_image_new(hsv->width,hsv->height,1,255);
+    IVC* bc = vc_image_new(hsv_s->width,hsv_s->height,1,255);
+    IVC* blobs = vc_image_new(bc->width,bc->height,1,255);
 
-    //system("cmd /c start FilterGear frame1.png");
-    system("FilterGear frame1.png");
+    int n = 7;
 
-    vc_image_free(image);
+    vc_rgb_to_hsv(src, hsv);
+    vc_hsv_segmentation(hsv, hsv_s, 15, 30, 50, 100, 25, 90);
+    vc_binary_close(hsv_s, bc, 7);
+    OVC* blob = vc_binary_blob_labelling(bc, blobs, &n);
+    vc_binary_blob_info(bc, blob, n);
+
+    vc_write_image("imagemfinal.ppm", blobs);
+
+    system("cmd /c start FilterGear frame10.ppm");
+    system("FilterGear imagemfinal.ppm");
+
+    vc_image_free(src);
+    vc_image_free(hsv);
+    vc_image_free(hsv_s);
+    vc_image_free(bc);
+    vc_image_free(blobs);
     return 1;
 }
